aa7badff49826eb3819e030473e7fd92f64ff398
AndualemGeta <andualemgetachew80@gmail.com>
2022-05-17 01:35:12 -0700

    adding tasks

--- _puts.c
+++ _puts.c
@@ -1,24 +1,24 @@
 #include "shell.h"
 #include <unistd.h>
 /*
- * _putchar - writes the character c to stdout
- * @c: The character to print
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
+* _putchar - writes the character c to stdout
+* @c: The character to print
+* Return: On success 1.
+* On error, -1 is returned, and errno is set appropriately.
+*/
 int _putchar(char c)
 {
-	return (write(1, &c, 1));
+return (write(1, &c, 1));
 }
 
 /**
- * _puts - puts function strings
- * @str: character value.
- */
+* _puts - puts function strings
+* @str: character value.
+*/
 void _puts(char *str)
 {
-	int i;
+int i;
 
-	for (i = 0; str[i] != 0; i++)
-	_putchar(str[i]);
+for (i = 0; str[i] != 0; i++)
+_putchar(str[i]);
 }
