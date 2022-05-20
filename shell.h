5d2cf704bd8f2ca3deda2522f33a9813697b03c9
AndualemGeta <andualemgetachew80@gmail.com>
2022-05-20 07:01:49 -0700

    fixing shell

--- shell.h
+++ shell.h
@@ -15,7 +15,40 @@
 
 /* Global environemnt */
 extern char **environ;
+/*
+* struct list_s - A new struct type defining a linked list.
+* @dir: A directory path.
+* @next: A pointer to another struct list_s.
+*/
+typedef struct list_s
+{
+char *dir;
+struct list_s *next;
+} list_t;
 
+/*
+* struct builtin_s - A new struct type defining builtin commands.
+* @name: The name of the builtin command.
+* @f: A function pointer to the builtin command's function.
+*/
+typedef struct builtin_s
+{
+char *name;
+int (*f)(char **argv, char **front);
+} builtin_t;
+
+/*
+* struct alias_s - A new struct defining aliases.
+* @name: The name of the alias.
+* @value: The value of the alias.
+* @next: A pointer to another struct alias_s.
+*/
+typedef struct alias_s
+{
+char *name;
+char *value;
+struct alias_s *next;
+} alias_t;
 /* Main Helpers */
 ssize_t _getline(char **lineptr, size_t *n, FILE *stream);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
