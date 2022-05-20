2875c09eef935031d5b3c887c5ded21b84a1b2ff
AndualemGeta <andualemgetachew80@gmail.com>
2022-05-20 06:41:40 -0700

    removing global

--- shell.h
+++ shell.h
@@ -15,48 +15,6 @@
 
 /* Global environemnt */
 extern char **environ;
-/* Global program name */
-char *name;
-/* Global history counter */
-int hist;
-
-/*
- * struct list_s - A new struct type defining a linked list.
- * @dir: A directory path.
- * @next: A pointer to another struct list_s.
- */
-typedef struct list_s
-{
-		char *dir;
-			struct list_s *next;
-} list_t;
-
-/*
- * struct builtin_s - A new struct type defining builtin commands.
- * @name: The name of the builtin command.
- * @f: A function pointer to the builtin command's function.
- */
-typedef struct builtin_s
-{
-		char *name;
-			int (*f)(char **argv, char **front);
-} builtin_t;
-
-/*
- * struct alias_s - A new struct defining aliases.
- * @name: The name of the alias.
- * @value: The value of the alias.
- * @next: A pointer to another struct alias_s.
- */
-typedef struct alias_s
-{
-	char *name;
-	char *value;
-	struct alias_s *next;
-} alias_t;
-
-/* Global aliases linked list */
-alias_t *aliases;
 
 /* Main Helpers */
 ssize_t _getline(char **lineptr, size_t *n, FILE *stream);
